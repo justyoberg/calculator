b043f76c9b1232dbe41578c2948b5a0a2da64a6a
Justy Oberg <justyoberg1@gmail.com>
2022-09-22 09:52:23 -0700

    Add numpad functionality and tweak some functions

--- scripts.js
+++ scripts.js
@@ -1,46 +1,41 @@
 const buttons = document.querySelectorAll("button");
 const screen = document.querySelector(".screen-text");
-const operatorList = ["+", "÷", "-", "x"]
+const numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operatorList = ["+", "/", "-", "*", ".", "=", "Enter"];
 
 var total = "";
 var input = "";
 var operator = null;
 
+
+// Main button click functionality
 buttons.forEach(button => button.addEventListener("click", function () {
-  
+
   if (!isNaN(button.textContent)) {
-
-    if (input[0] == "0") {
-      input = button.textContent;
-      updateScreen(input);
-    } else {
-      input += button.textContent;
-      updateScreen(input)
-    }
-   
+    inputNumbers(button);
   } else if (isNaN(button.textContent)) {
+    inputOperator(button);
+  }  
 
-    if (button.textContent === "c") return clear();
-    else if (button.textContent === ".") return addDecimal();
-    else if (operatorList.includes(button.textContent)) {
-      if (operator === null) {
-        operator = button.textContent;
-        total += input;
-        input = "";
-      } else {
-        calculate();
-        operator = button.textContent;
-      }
-    }
-    else if (button.textContent === "=") return calculate();
-  }
 }));
+
+
+// Main keyboard input functionality
+window.addEventListener("keydown", function (e) {
+  
+  if (numberList.includes(e.key)) {
+    keyboardInputNum(e.key);
+  } else if (operatorList.includes(e.key)) {
+    keyboardInputOper(e.key);
+  }
+
+});
 
 function operate(operator, x, y) {
   if (operator === "+") return add(x, y);
   else if (operator === "-") return subtract(x, y);
-  else if (operator === "x") return multiply(x, y);
-  else if (operator === "÷") return divide(x, y);
+  else if (operator === "*") return multiply(x, y);
+  else if (operator === "/") return divide(x, y);
 }
 
 function add(x, y) {
@@ -105,4 +100,65 @@ function updateScreen(value) {
   } else {
     screen.textContent = value.toString();
   }
+}
+
+
+function inputNumbers(button) {
+
+  if (input[0] == "0") {
+    input = button.textContent;
+    updateScreen(input);
+  } else {
+    input += button.textContent;
+    updateScreen(input)
+  }
+}
+
+function inputOperator(button) {
+
+  if (button.textContent === "c") return clear();
+    else if (button.textContent === ".") return addDecimal();
+    else if (operatorList.includes(button.textContent)) {
+      
+      if (operator === null) {
+        operator = button.textContent;
+        total += input;
+        input = "";
+      } else {
+        calculate();
+        operator = button.textContent;
+      }
+    
+    }
+    else if (button.textContent === "=") return calculate();
+}
+
+function keyboardInputNum(key) {
+
+  if (input[0] == "0") {
+    input = key;
+    updateScreen(input);
+  } else {
+    input += key;
+    updateScreen(input)
+  }
+}
+
+function keyboardInputOper(key) {
+  
+  if (key === "c") return clear();
+    else if (key === ".") return addDecimal();
+    else if (operatorList.includes(key)) {
+      
+      if (operator === null) {
+        operator = key;
+        total += input;
+        input = "";
+      } else {
+        calculate();
+        operator = key;
+      }
+    
+    }
+    else if (key === "Enter") return calculate();
 }
